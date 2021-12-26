1d2faf8d00462ee0cb1abb4a5e17d00f7b46f04e
Misavan <kh.van0618@gmail.com>
2021-12-26 22:38:49 +0700

    Van

---
+++ datvexemphim/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <DatVeXemPhim/>
+    </div>
+  );
+}
+
+export default App;
---
+++ datvexemphim/src/DatVeXemPhim/main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DatVeXemPhim extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Đặt vé xem phim</h2>
+            </div>
+        )
+    }
+}
